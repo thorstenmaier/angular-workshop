472003e03543675758360e3d3bf8fc28fcd3e5c6
Thorsten Maier <Thorsten.Maier@oio.de>
2021-02-03 08:54:44 +0100

    Excel Beispiel

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { combineLatest } from "rxjs";
+import { debounce, debounceTime, delay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-reactive',
-  templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  selector: "app-reactive",
+  templateUrl: "./reactive.component.html",
+  styleUrls: ["./reactive.component.css"],
 })
 export class ReactiveComponent implements OnInit {
-
-
+  aFormControl = new FormControl();
+  bFormControl = new FormControl();
+  resultFormControl = new FormControl();
 
   ngOnInit(): void {
+    combineLatest([
+      this.aFormControl.valueChanges.pipe(debounceTime(300)),
+      this.bFormControl.valueChanges,
+    ]).subscribe(([a, b]) => {
+      this.resultFormControl.setValue(a + b);
+    });
   }
-
 }
