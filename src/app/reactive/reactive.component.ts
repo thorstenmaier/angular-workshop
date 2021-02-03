98cbc07e96dd4e436a7473dcc752ec3d89a8a28c
Thorsten Maier <Thorsten.Maier@oio.de>
2021-02-03 14:07:22 +0100

    Finale Observable Lösung Workshop Morgen 3.2.2021

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { FormControl } from "@angular/forms";
-import { combineLatest, interval, Observable, of, zip } from "rxjs";
-import { debounce, debounceTime, delay } from "rxjs/operators";
+import { combineLatest, interval, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-reactive",
   templateUrl: "./reactive.component.html",
   styleUrls: ["./reactive.component.css"],
 })
-export class ReactiveComponent implements OnInit {
+export class ReactiveComponent {
   aFormControl = new FormControl();
   bFormControl = new FormControl();
   resultFormControl = new FormControl();
 
-  ngOnInit(): void {
-    combineLatest([
+  countObservable$: Observable<number>;
+
+  result2$: Observable<number>;
+
+  constructor() {
+    this.countObservable$ = interval(1000);
+
+    const resultObservable$ = combineLatest([
       this.aFormControl.valueChanges,
       this.bFormControl.valueChanges,
-    ]).subscribe(([a, b]) => {
-      this.resultFormControl.setValue(a + b);
+    ]).pipe(map(([a, b]) => a + b));
+
+    resultObservable$.subscribe((result) => {
+      this.resultFormControl.setValue(result);
     });
+
+    this.result2$ = combineLatest([
+      this.countObservable$,
+      resultObservable$,
+    ]).pipe(map(([count, result]) => count * result));
   }
 }
