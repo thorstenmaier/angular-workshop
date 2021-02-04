e857a0efd7dd575abd65837f7e8fcfc5b0a01ec2
Thorsten Maier <Thorsten.Maier@oio.de>
2021-02-04 09:31:08 +0100

    Übung

--- src/app/_components/tradername/tradername.component.ts
+++ src/app/_components/tradername/tradername.component.ts
@@ -9,17 +9,9 @@ import { debounceTime, filter, flatMap, map, startWith } from "rxjs/operators";
   styleUrls: ["./tradername.component.css"],
 })
 export class TradernameComponent implements OnInit {
-  tradernameExists$: Observable<boolean>;
-  tradernameChangedSubject = new Subject<string>();
+  tradernameExists = true;
 
   constructor(private traderService: TradernameService) {}
 
-  ngOnInit(): void {
-    this.tradernameExists$ = this.tradernameChangedSubject.pipe(
-      debounceTime(200),
-      flatMap((input) => this.traderService.isTradernameExisting(input)),
-      map((te) => te.exists),
-      startWith(true)
-    );
-  }
+  ngOnInit(): void {}
 }
